33611450b1553956c116d609d5d73c77a23f5310
Karthika K <169062414+KARTHIKA1836@users.noreply.github.com>
2026-03-19 10:30:02 +0530

    Time: 1 ms (61.77%), Space: 43.1 MB (80.24%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,15 +1,14 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        HashSet<Character> jewelSet = new HashSet<>();
-        for(char jewel : jewels.toCharArray()){
-            jewelSet.add(jewel);
-        }
+       
         int count = 0;
+      for(char jewel : jewels.toCharArray()){
         for(char stone : stones.toCharArray()){
-            if(jewelSet.contains(stone)){
+            if(jewel==stone){
                 count++;
             }
         }
+      }
         return count;
     }
 }
